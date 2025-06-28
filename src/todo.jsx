15cffda2633d026b739ff4b241ea93d7dafe1bd6
saunaman37 <ptiger1015@gmail.com>
2025-06-29 08:31:36 +0900

    JSXで構造を作成

---
+++ src/todo.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export const  Todo = () =>
+{
+  return(
+    <>
+    <div>
+      <input placeholder='Todoを入力'/>
+      <button>追加</button>
+    </div>
+    <div>
+      <p>未完了のTODO</p>
+      <ul>
+        <li>
+          <p>TODOです</p>
+          <button>完了</button>
+          <button>削除</button>
+        </li>
+        <li>
+          <p>TODOです</p>
+          <button>完了</button>
+          <button>削除</button>
+        </li>
+      </ul>
+    </div>
+    <div>
+    <p>完了のTODO</p>
+      <ul>
+        <li>
+          <p>TODOでした</p>
+          <button>戻す</button>
+        </li>
+        <li>
+          <p>TODOでした</p>
+          <button>戻す</button>
+        </li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default Todo
