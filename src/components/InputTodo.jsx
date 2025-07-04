5c4aa8dc1512f6a728cff6fc61b66743c07190ea
saunaman37 <ptiger1015@gmail.com>
2025-07-04 17:57:46 +0900

    コンポーネント内でのスタイルの定義

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,17 @@
+const style ={
+  backgroundColor: "rgb(0,255,247)",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+}
+
+
 export const InputTodo = (props)=>{
     const {todoText,onChange,onClick} = props;
     return(
-      <div className='input-area'>
+      <div  style={style}>
         <input 
           placeholder='Todoを入力' 
           value={todoText} 
